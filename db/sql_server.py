c4b9ef1a7cfbe895dcfde51380586eb804f26328
hunterlang <hunterlang@comcast.net>
2011-07-11 12:33:18 -0400

    asdf

---
+++ db/sql_server.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2011 Hunter Lang and Avi Romanoff
+# AGPL 3 License. See LICENSE.
+from tornado.options import define, options
+import tornado.template as template
+from tornado.escape import json_encode
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import tornado.escape as escape
+import sqlite3 as dbmod
+
+define("port", default=9999, help="run on the given port", type=int)
+
+dbc = dbmod.connect('db.db')
+db = dbc.cursor()
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/view/children/global_topic/([A-Za-z0-9\-\.\_\%]+)", ViewGlobalLocalesHandler),
+            (r"/view/children/global_locale/([A-Za-z0-9\-\.\_\%]+)", ViewTopicsHandler),
+            (r"/view/childre/topic/([A-Za-z0-9\-\.\_\%]+)", ViewLocalesHandler),
+            (r"/view/categories", ViewCategoriesHandler),
+        ]
+        
+        settings = dict(
+            debug= True,
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+
+class ViewGlobalLocalesHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    def get(self, global_topic):
+        db.execute('select _id from global_topic where name = ?', [global_topic])
+        topic_id = db.fetchone()[0]
+        db.execute('select name from global_locale where parent_id = ?', [topic_id])
+        glob_locales = db.fetchall()
+        finish = {
+            "parent": global_topic,
+            "children": glob_locales
+        }
+        
+        self.finish(finish)
+
+class ViewTopicsHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    def get(self, raw_data):
+        arr = escape.url_unescape(raw_data).split("|")
+        global_locale = arr[1]
+        global_topic = arr[0]
+        db.execute('select _id from global_topic where name = ?', [global_topic])
+        topic_id = db.fetchone()[0]
+        db.execute('select _id from global_locale where parent_id = ?', [topic_id])
+        locale_id = db.fetchone()[0]
+        db.execute('select name from topic where parent_id = ?', [locale_id])
+        topics = db.fetchall()
+        finish = {
+            "parent": global_topic,
+            "children": topics
+        }
+        
+        self.finish(finish)
+
+class ViewLocalesHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    def get(self, raw_data):
+        arr = escape.url_unescape(raw_data).split("|")
+        topic = arr[2]
+        global_locale = arr[1]
+        global_topic = arr[0]
+        db.execute('select _id from global_topic where name = ?', [global_topic])
+        topic_id = db.fetchone()[0]
+        db.execute('select _id from global_locale where parent_id = ? and name = ?', [topic_id, global_locale])
+        locale_id = db.fetchone()[0]
+        db.execute('select _id from topic where parent_id = ? and name = ?', [locale_id, topic])
+        topic_id = db.fetchone()[0]
+        db.execute('select name from locale where parent_id = ?', [topic_id])
+        locales = db.fetchall()
+        finish = {
+            "parent": global_topic,
+            "children": topics
+        }
+        
+        self.finish(finish)
+
+class ViewCategoriesHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    def get(self):
+        results = []
+        db.execute('select name from global_topic')
+        names = db.fetchall()
+        for name in names:
+            results.append(name[0])
+        print results
+        self.finish(str(results))
+
+def main():
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application(), xheaders=True)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == '__main__':
+    main()
